a36d869913c4d74b634c8444c51831d71699d25b
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-09-02 15:44:06 -0300

    Adicionado o Swagger com title, descripiton e version para documentar a api que esta sendo feita

--- src/main/java/Api/Constructor/Version1/Swagger/CadastroSwaggerConfig.java
+++ src/main/java/Api/Constructor/Version1/Swagger/CadastroSwaggerConfig.java
@@ -1,15 +1,21 @@
 package Api.Constructor.Version1.Swagger;
 
+import Api.Constructor.Version1.database.model.Cadastro;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+
 
 @Configuration
 public class CadastroSwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI(){
-
-        return null;
+        return new OpenAPI().info(new Info()
+                .title("Cadastro API")
+                .description("API para gerenciamento de cadastros")
+                .version("1.0"));
     }
 }
