212ab87e28056d8044c86759a2c9c61240c21d66
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-09-03 10:56:18 -0300

    comecando a adicionar a validacao para os dtos da api com name sendo adicionao @Size e @NotBlank

--- src/main/java/Api/Constructor/Version1/dto/CadastroDto.java
+++ src/main/java/Api/Constructor/Version1/dto/CadastroDto.java
@@ -1,7 +1,13 @@
 package Api.Constructor.Version1.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record CadastroDto(
 
+        @NotBlank(message = "Name is mandatory")
+        @Size(min = 4, max = 50, message = "Name must be between 4 and 50 characters")
         String name,
 
         String email,
