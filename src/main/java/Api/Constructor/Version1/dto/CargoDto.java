996f3456a5a094bfa42f35b0fd1caa43e0474ad2
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-09-11 16:18:15 -0300

    reformulando algumas coisas no validation

--- src/main/java/Api/Constructor/Version1/dto/CargoDto.java
+++ src/main/java/Api/Constructor/Version1/dto/CargoDto.java
@@ -1,15 +1,12 @@
 package Api.Constructor.Version1.dto;
 
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 
 public record CargoDto(
 
-
+        @Size(min = 5, message = "Invalid position")
         @NotBlank(message = "Defining the type of position is mandatory.")
         String nomeCargo,
 
@@ -19,6 +16,7 @@ public record CargoDto(
         @DecimalMin(value = "0.1", message = "Salary must be a positive number or zero")
         BigDecimal salarioCargo,
 
+        @Size(max = 200, message = "Do not exceed the maximum character limit.")
         @NotBlank(message = "Provide the job specifications.")
         String descricaoCargo)
 {
